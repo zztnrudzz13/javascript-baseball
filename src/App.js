f5898edac8996a174a2abd13c7818e59d4d6a358
Sukyeong Park <zztnrudzz13@naver.com>
2022-11-04 12:35:40 +0900

    feat($app): implement initialize function print start message and get computer random number

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  #answer;
+
+  #getComputerNumber() {
+    const randomArray = MissionUtils.Random.pickUniqueNumbersInRange(0, 9, 3);
+    const randomNumber = randomArray.join('');
+    return randomNumber;
+  }
+
+  #initialize() {
+    const START_MESSAGE = '숫자 야구게임을 시작합니다';
+    MissionUtils.Console.print(START_MESSAGE);
+    this.#answer = this.#getComputerNumber();
+  }
+
+  play() {
+    this.#initialize();
+  }
 }
 
 module.exports = App;
