1489f39e867f048c6bb044532d6077d872b8581d
Sukyeong Park <zztnrudzz13@naver.com>
2022-11-04 11:47:30 +0900

    style(eslint, prettier): initial setting

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+        endOfLine: 'auto',
+      },
+    ],
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+    'max-depth': ['error', 2],
+    'max-lines-per-function': ['error', 15],
+    'spaced-comment': ['error', 'always'],
+    'no-new': 'off',
+    'no-alert': 'off',
+    'no-console': 'off',
+    'no-plusplus': 'off',
+    'class-methods-use-this': 'off',
+    'lines-between-class-members': 'off',
+  },
+};
