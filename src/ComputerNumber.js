7326fa1802e11e558b6b12c762b8825b2e31a5f0
Sukyeong Park <zztnrudzz13@naver.com>
2022-11-25 13:23:48 +0900

    feat(ComputerNumber): implement generating computer number

---
+++ src/ComputerNumber.js
@@ -0,0 +1,16 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const ComputerNumber = {
+  generate() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  },
+};
+
+module.exports = ComputerNumber;
