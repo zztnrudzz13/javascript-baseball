61e83439fca9f40ceaa42a935af85a99e87fee6e
Sukyeong Park <zztnrudzz13@naver.com>
2022-11-26 11:59:11 +0900

    feat(InputView): implement readGameCommand function

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -4,6 +4,13 @@ const InputView = {
   readGameNumber(callback) {
     MissionUtils.Console.readLine('숫자를 입력해 주세요 : ', callback);
   },
+
+  readGameCommand(callback) {
+    MissionUtils.Console.readLine(
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
+      callback,
+    );
+  },
 };
 
 module.exports = InputView;
